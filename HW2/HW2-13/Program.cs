45596349456041475c315039581301bd9083e57b
dmitry <shkladmi@gmaile.com>
2022-11-30 23:22:13 +0300

    Hw2END

---
+++ HW2/HW2-13/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+Задача 13: Напишите программу, которая выводит третью цифру 
+заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+*/
+
+int GetNumer(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+    
+    while(!isCorrect)
+    
+    {
+        Console.Write(message+" ");
+
+        if(int.TryParse(Console.ReadLine(), out result))
+            isCorrect = true;
+        else
+            Console.WriteLine("Вы ввели не корректное число. \n");
+    }
+    
+    return result;
+}
+
+int num = GetNumer("Введите  число:");
+ if (num <100)
+ {
+    Console.WriteLine("третьей цифры нет.");
+ }
+ else
+  {
+    Console.WriteLine(num%1000/100);
+  }
---
+++ HW2/HW2-15/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задача 15: Напишите программу, которая принимает на вход цифру,
+ обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+int GetNumer(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+    
+    while(!isCorrect)
+    
+    {
+        Console.Write(message+" ");
+
+        if(int.TryParse(Console.ReadLine(), out result) && result>0 && result<8)
+            isCorrect = true;
+        else
+            Console.WriteLine("Вы ввели не корректное число. \n");
+    }
+    
+    return result;
+}
+
+int num = GetNumer("Введите день недели (число от 1 до 7):");
+if(num == 6 || num == 7)Console.WriteLine("да");
+
+else Console.WriteLine("нет");
