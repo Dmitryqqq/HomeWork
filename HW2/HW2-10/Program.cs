291d0549ad134e87f7cd34f4ee008ac4c9300bbb
dmitry <shkladmi@gmaile.com>
2022-11-30 22:53:39 +0300

    Hw2

---
+++ HW2/HW2-10/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+ и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int GetNumer(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+    
+    while(!isCorrect)
+    
+    {
+        Console.Write(message+" ");
+
+        if(int.TryParse(Console.ReadLine(), out result) && result>99 && result<1000)
+            isCorrect = true;
+        else
+            Console.WriteLine("Вы ввели не корректное число. \n");
+    }
+    
+    return result;
+}
+
+int num = GetNumer("Введите трёхзначное число:");
+Console.WriteLine(num%100/10);
